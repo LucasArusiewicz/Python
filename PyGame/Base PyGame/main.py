bfd415d9bc9533035e5980b6c1e6bbff1251ce38
LucasArusiewicz <lucasarusiewicz@gmail.com>
2017-12-04 00:48:28 -0200

    Janela

---
+++ PyGame/Base PyGame/main.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('PyGame')
+    clock = pygame.time.Clock()
+    crashed = False
+
+    while not crashed:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+
+            print(event)
+
+        pygame.display.update()
+        clock.tick(60)
+    pygame.quit()
+    quit()
+
+if __name__ == '__main__':
+    main();
